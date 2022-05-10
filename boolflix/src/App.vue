3ef653bf5a3e895c537afc3b6288cc11613002c2
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-10 12:18:42 +0200

    Add img and integer number

--- boolflix/src/App.vue
+++ boolflix/src/App.vue
@@ -1,32 +1,38 @@
-/* Milestone 1:
-Creare un layout base con una searchbar (una input e un button) in cui possiamo
-scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
-film trovato:
-1. Titolo
-2. Titolo Originale
-3. Lingua
-4. Voto */
+ /*Milestone 3:
+In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
+al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
+perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+Dovremo prendere quindi l’URL base delle immagini di TMDB:
+https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+(troviamo tutte le dimensioni possibili a questo link:
+https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la
+parte finale dell’URL passata dall’API.
+Esempio di URL:
+https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+Trasformiamo poi il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+piene (o mezze vuote :P)*/
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style> */
 
- /* 
- Milestone 2:
-Trasformiamo la stringa statica della lingua in una vera e propria bandiera della
-nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
-nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
-dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
-attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
-risposta diversi, simili ma non sempre identici)
-Qui un esempio di chiamata per le serie tv:
-https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
-crub
-  */
+
 
 <template>
   <div id="app">
     <header>
-      <div class="logo"></div>
+      <div class="logo">
+        <img src="@/assets/img/logo_boolflix.png" alt="logo">
+      </div>
       <div class="search-film">
         <form action="">
           <label for="">Search Film</label>
@@ -45,6 +51,7 @@ crub
     <main>
       <div class="dati_movies" v-for="movie in mixed" :key="movie.id">
         <ul>
+          <li><img :src="'https://image.tmdb.org/t/p/w200/' + movie.poster_path" alt="copertina"></li>
           <li v-if="movie.title">Titolo Film: {{ movie.title }}</li>
           <li v-else>Titolo Serie: {{ movie.name }}</li>
 
@@ -81,7 +88,7 @@ crub
             Lingua: <flag iso="pt" />
           </li>
           <li v-else>Lingua: {{ movie.original_language }}</li>
-          <li>Voto: {{ movie.vote_average }}</li>
+          <li>{{Math.ceil(movie.vote_average)}}</li>
         </ul>
       </div>
 
@@ -137,6 +144,7 @@ export default {
       serie_API:
         "https://api.themoviedb.org/3/search/tv?api_key=feeebc687dcbe2134ac810d7cb75dafe&language=it_IT&query=",
         mixed: null,
+        vote: null,
     };
   },
 
@@ -159,6 +167,7 @@ export default {
         }));
     },
 
+
     /* call_API_series() {
       axios
       .get(this.serie_API + this.titolo)
@@ -169,7 +178,21 @@ export default {
     }, */
   },
 
-  mounted() {},
+  mounted() {
+    
+  },
+
+ /*  computed: {
+    convertNumber(){
+      this.mixed.forEach(element => {
+        let vote = element.vote_average
+        //return console.log(Math.floor(vote));
+        let star_vote = Math.floor(vote)
+        return console.log(star_vote);
+        //return this.vote = star_vote
+      });
+    }
+  } */
 };
 </script>
 
